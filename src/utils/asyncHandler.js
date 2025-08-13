0505384aa42c31a5bab36758604101c098622aa0
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-08-13 10:48:49 +0530

    utils code discussion

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,3 +1,9 @@
-const asyncHandler = () => {};
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((error) => {
+      next(error);
+    });
+  };
+};
 
 export { asyncHandler };
