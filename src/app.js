303a66c267592ade3da45ea26319605ab83089b2
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-08-13 09:55:01 +0530

    server set

---
+++ src/app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+// origin → Only allows requests from the frontend domain specified in CORS_ORIGIN (from .env).
+// credentials: true → Allows cookies, authorization headers, or TLS client certificates to be sent across domains.
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+
+// Parses incoming JSON requests.
+// Limits payload size to 16 KB (to prevent large data abuse).
+// Parses URL-encoded form data (application/x-www-form-urlencoded).
+// extended: true → Allows nested objects in data.
+// Also limited to 16 KB.
+// Serves files (images, CSS, JS) from the public folder.
+// Example: If you have /public/logo.png, you can access it at http://yourserver.com/logo.png.
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import dotenv from "dotenv";
-
 import connctDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
 
-connctDB();
+connctDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`server is running at port: ${process.env.PORT} `);
+    });
+  })
 
-// import express from "express";
-// const app = express();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`);
-//     app.on("error", (error) => {
-//       console.log("error");
-//       throw err;
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listing port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.log("error", error);
-//     throw err;
-//   }
-// })();
+  .catch((error) => {
+    console.log("Monogo connection failed");
+  });
