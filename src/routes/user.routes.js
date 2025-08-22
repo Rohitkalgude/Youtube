798defd369892f19e6fe87f767be941c9c0b455b
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-08-22 17:29:37 +0530

    Add user profile management routes and fix typo in watch history endpoint

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,13 @@ import {
   logoutUser,
   register,
   refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  UpdateAccount,
+  UpdateUseravatar,
+  UpdateUsercoverImage,
+  getUserChannelProfile,
+  getWatchHistroy,
 } from "../controllers/user.controllers.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
@@ -21,7 +28,14 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 
 router.route("/logout").post(verfiyJWT, logoutUser);
-
 router.route("/refresh-Token").post(refreshAccessToken);
+router.route("/change-password").post(verfiyJWT, changeCurrentPassword);
+router.route("/current-user").get(verfiyJWT, getCurrentUser);
+router.route("/update-Account").patch(verfiyJWT, UpdateAccount);
+router.route("/avatar").patch(verfiyJWT, upload.single("avatar"), UpdateUseravatar);
+router.route("/cover-image").patch(verfiyJWT, upload.single("coverImage"), UpdateUsercoverImage);
+router.route("/c/:username").get(verfiyJWT, getUserChannelProfile);
+router.route("/histroy").get(verfiyJWT, getWatchHistroy)
+
 
 export default router;
